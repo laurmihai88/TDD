40f3d3333fb87c5435600ea7da465c2383cd4264
laurmihai88 <panaete.laurentiu@gmail.com>
2020-02-13 11:25:28 +0200

    TDD pushed

---
+++ src/test/java/com/laurentiu/CalculatorTest.java
@@ -0,0 +1,50 @@
+package com.laurentiu;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    Calculator calculator = new Calculator();
+
+    @Test
+    public void whenAdd_returnOk() {
+
+        int expected = 5;
+        int actual = calculator.add(2, 3);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void whenSubstract_returnOk() {
+        int expected = 6;
+        int actual = calculator.subtract(20, 14);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void whenDivide_returnOk() {
+        expectedException.expect(ArithmeticException.class);
+        expectedException.expectMessage("/ by zero");
+        int number = new Random().nextInt();
+
+        calculator.divide(number,0);
+    }
+
+    @Test
+    public void whenMultiply_returnOk(){
+        int expected = 20;
+        int actual = calculator.multiply(4,5);
+
+        assertEquals(expected,actual);
+    }
+
+}
